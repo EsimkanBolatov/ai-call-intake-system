e6063a9242fe0eb8efd42832983804ed9bd13c93
EsimkanBolatov <esi.bolat4@gmail.com>
2026-01-16 23:24:18 +0500

    fix

--- frontend/src/pages/CallSimulator/CallSimulator.tsx
+++ frontend/src/pages/CallSimulator/CallSimulator.tsx
@@ -5,47 +5,6 @@ import {
 import { Mic, PhoneDisabled, RecordVoiceOver, SettingsVoice } from "@mui/icons-material";
 import { io, Socket } from "socket.io-client";
 
-// Хелперы для WAV
-// const audioBufferToWav = (buffer: AudioBuffer) => {
-//   const numChannels = 1;
-//   const sampleRate = buffer.sampleRate;
-//   const format = 1; // PCM
-//   const bitDepth = 16;
-//   const bytesPerSample = bitDepth / 8;
-//   const blockAlign = numChannels * bytesPerSample;
-//   const samples = buffer.getChannelData(0);
-//   const dataLength = samples.length * bytesPerSample;
-//   const bufferLen = 44 + dataLength;
-//   const arrayBuffer = new ArrayBuffer(bufferLen);
-//   const view = new DataView(arrayBuffer);
-
-//   const writeString = (view: DataView, offset: number, string: string) => {
-//     for (let i = 0; i < string.length; i++) view.setUint8(offset + i, string.charCodeAt(i));
-//   };
-
-//   writeString(view, 0, 'RIFF');
-//   view.setUint32(4, 36 + dataLength, true);
-//   writeString(view, 8, 'WAVE');
-//   writeString(view, 12, 'fmt ');
-//   view.setUint32(16, 16, true);
-//   view.setUint16(20, format, true);
-//   view.setUint16(22, numChannels, true);
-//   view.setUint32(24, sampleRate, true);
-//   view.setUint32(28, sampleRate * blockAlign, true);
-//   view.setUint16(32, blockAlign, true);
-//   view.setUint16(34, bitDepth, true);
-//   writeString(view, 36, 'data');
-//   view.setUint32(40, dataLength, true);
-
-//   let offset = 44;
-//   for (let i = 0; i < samples.length; i++) {
-//     let s = Math.max(-1, Math.min(1, samples[i]));
-//     view.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
-//     offset += 2;
-//   }
-//   return new Blob([arrayBuffer], { type: 'audio/wav' });
-// };
-
 const arrayBufferToBase64 = (buffer: ArrayBuffer): string => {
   let binary = '';
   const bytes = new Uint8Array(buffer);
